21ba12b911ba81d8b059e3a3734708e470fb5586
dengzhiping <dengzhiping@MacBook-Pro.local>
2016-11-29 11:02:11 +0800

    commit

---
+++ AutoTest/AutoTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  AutoTest
+//
+//  Created by dengzhiping on 2016/11/28.
+//  Copyright © 2016年 dengzhiping. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"aaa");
+    NSLog(@"bbb");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
